f5a09933a756e2605e569782bc93a59cc718798c
Ebernn <r1.bernier@orange.fr>
2021-06-07 22:45:33 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Anonymous starter 🐱‍👤
+
+Simple typescript boilerplate for puppeteer + tor projects.
+
+## Setup
+
+You need : tor, node, yarn (or npm)
+
+Install the depedencies : `yarn`
+
+Configure [socks proxy on your machine](https://superuser.com/questions/1528185/how-can-i-set-socks-proxy-on-windows)
+
+## Run it
+
+`yarn run start`
---
+++ app.ts
@@ -0,0 +1,27 @@
+import puppeteer from 'puppeteer';
+import colors from 'colors/safe';
+
+(async () => {
+	const args = ['--proxy-server=socks5://localhost:9051'];
+	const browser = await puppeteer.launch({ args });
+	const page = await browser.newPage();
+	await page.goto('https://check.torproject.org/');
+	const isUsingTor = await page.$eval('body', el =>
+		el.innerHTML.includes('Congratulations. This browser is configured to use Tor')
+	);
+
+	if (!isUsingTor) {
+		console.log(colors.red('Not using Tor\nClosing...'));
+		return await browser.close();
+	}
+
+	console.log(colors.green('Using Tor 🧅\nContinuing... '));
+
+	// Now you can go wherever you want
+	await page.goto('https://propub3r6espa33w.onion/');
+
+	// You would add additional code to do stuff... 
+
+	// Then when you're done, just close
+	await browser.close();
+})();
